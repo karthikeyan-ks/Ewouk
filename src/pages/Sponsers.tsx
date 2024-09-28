f94567e42a01d48fe02b86d9eeb3d0c05b71b68e
SUFIYANJT <jtfinu@gmail.com>
2024-09-28 21:37:22 +0530

    cool

--- src/pages/Sponsers.tsx
+++ src/pages/Sponsers.tsx
@@ -1,16 +1,33 @@
-import './Team.css'; // Import the CSS file for styling
+import './Sponsers.css'; // Import the CSS file for styling
 import logo from '../static/image/image.png';
-export const Sponsers =() => {
-  
-    
+import realwebLogo from '../static/image/realweb.png'; // Import realweb logo
+import teklearnLogo from '../static/image/TEKLEARN.png'; // Import Teklearn logo
 
-        return (
-            <div className="team-container">
-                <div className='image-container1'>
-                    <img src={logo} alt="Ewouk-logo" />
-                </div>
-                {/* Other content goes here */}
+const sponsorsList = [
+    { name: 'REALWEB', image: realwebLogo },
+    { name: 'TEKLEARN', image: teklearnLogo },
+    // Add more sponsors here
+];
+
+export const Sponsers = () => {
+    return (
+        <div className="team-container">
+            <div className='image-container1'>
+                <img src={logo} alt="Ewouk-logo" />
             </div>
-        );
-    };
-    
+            <div className='heading'>
+                <h1>OUR SPONSORS</h1>
+            </div>
+            <div className="sponsors-grid">
+                {sponsorsList.map((sponsor, index) => (
+                    <div key={index} className="sponsor-card1">
+                        <div className='sponsercard'>
+                        <img src={sponsor.image} alt={sponsor.name} className="sponsor-logo" />
+                        </div>
+                        <p className="sponsor-name">{sponsor.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
